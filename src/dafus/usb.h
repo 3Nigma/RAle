703ef8531d5602f99d0c22b30284b59a14861ba0
Victor ADĂSCĂLIȚEI <adascalitei.victor@gmail.com>
2012-09-17 16:48:12 +0300

    Update src/dafus/usb.h
    Done mostly formating edits. Be advised that since the browser was used to make these edits, they weren't tested!

--- src/dafus/usb.h
+++ src/dafus/usb.h
@@ -1,28 +1,34 @@
-// ======================================================================
-// Public interface of the USB driver
-//
-// Copyright 2006-2008 Dick Streefland
-//
-// This is free software, licensed under the terms of the GNU General
-// Public License as published by the Free Software Foundation.
-// ======================================================================
+/* ======================================================================
+ * Public interface of the USB driver
+ *
+ * Copyright 2006-2008 Dick Streefland
+ *
+ * This is free software, licensed under the terms of the GNU General
+ * Public License as published by the Free Software Foundation.
+ * ====================================================================== */
 
 #ifndef USB_H
-#define	USB_H
+#define USB_H
 
+/* define frequent used types */
 typedef	unsigned char	byte_t;
 typedef	unsigned int	uint_t;
 
-// usb.c
-extern	void		usb_init ( void );
-extern	void		usb_poll ( void );
+/* functions implemented in 'usb.c' */
+/* Initialize the low-level USB driver */
+extern void	usb_init(void);
+/* Poll USB driver:
+* - check for incoming USB packets
+* - refill an empty transmit buffer
+* - check for USB bus reset */
+extern void	usb_poll(void);
 
-// crc.S
-extern	void		crc ( byte_t* data, byte_t len );
+/* functions implemented in 'crc.S' */
+extern void crc(byte_t* data, byte_t len);
 
-// application callback functions
-extern	byte_t		usb_setup ( byte_t data[8] );
-extern	void		usb_out ( byte_t* data, byte_t len );
-extern	byte_t		usb_in ( byte_t* data, byte_t len );
+/* application callback functions */
+extern byte_t usb_setup(byte_t data[8]);
+extern void usb_out(byte_t* data, byte_t len);
+extern byte_t usb_in(byte_t* data, byte_t len);
 
-#endif	// USB_H
+#endif
