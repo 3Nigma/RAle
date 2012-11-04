352108944e358ee61192507069f1dd513a29eda8
Victor ADASCALITEI <adascalitei.victor@gmail.com>
2012-11-04 13:20:43 +0200

    [psale work] Cleaned up the main Makefile and added other makefile links to acomodate for the updater. An initial 'main' module was provided for the updater to test all of these.

---
+++ src/psale/reinoitor/main.c
@@ -0,0 +1,22 @@
+/*
+ * psAle - If Ale's board ever needed an app, then this would be IT!
+ * Copyright (c) 2012 by Victor ADĂSCĂLIȚEI at admin@tuscale.ro
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ , read it from the 
+ * COPYING file that resides in the same directory as this file does or send a letter to 
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
+
+#include <gtk/gtk.h>
+
+//#include "dl.h"
+
+int main(int argc, char *argv[]) {
+  gtk_init (&argc, &argv);
+  
+  gtk_main();
+  //dl_curata();
+
+  return 0;
+}
