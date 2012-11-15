4b894308483f7751f2c0f46846a1aad1387e8ad7
Victor ADASCALITEI <adascalitei.victor@gmail.com>
2012-11-15 22:08:25 +0200

    [psale work] Added check and upgrade local rpsale from psale:main.

--- src/psale/principal/main.c
+++ src/psale/principal/main.c
@@ -9,12 +9,41 @@
  */
 
 #include <gtk/gtk.h>
+#include <glib/gstdio.h>
 
 #include "fprin.h"
+
+#define ACTUALIZATOR_NUME_ACTUALIZAT "rpsale.nou"
+#ifdef G_OS_WIN32
+  #define ACTUALIZATOR_NUME_AP "rpsale.exe"
+#elif defined G_OS_UNIX
+  #define ACTUALIZATOR_NUME_AP "rpsale"
+#endif
+
+static gboolean
+incearca_actualizare_actualizator() {
+  gboolean rezultatOp = FALSE;
+  
+  if(g_file_test(ACTUALIZATOR_NUME_ACTUALIZAT, G_FILE_TEST_EXISTS)) {
+    /* există o versiune mai nouă a acutalizatorului descărcată.
+     * Înlocuiește actualizatorul curent cu cel nou */
+     if(g_remove(ACTUALIZATOR_NUME_AP) == 0) {
+	   if(g_rename(ACTUALIZATOR_NUME_ACTUALIZAT, ACTUALIZATOR_NUME_AP) == 0) {
+	     rezultatOp = TRUE;
+	   }
+	 }
+  }
+
+  return rezultatOp;
+}
 
 int main(int argc, char *argv[]) {
   FormularPrincipal *fprin = NULL;
 
+  if(incearca_actualizare_actualizator()) {
+    g_debug("Am realizat cu succes înlocuirea actualizatorului cu o versiune mai nouă.");
+  }
+  
   gtk_init (&argc, &argv);
   
   fprin = fp_initializeaza_formular_principal();
