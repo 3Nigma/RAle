95ad11b65923019f78f78791adb7920a96d30909
Victor ADĂSCĂLIȚEI <adascalitei.victor@gmail.com>
2012-12-28 22:57:44 +0200

    [rpsale work] Added copyright notice.

--- src/psale/reinoitor/par_aplicatie.c
+++ src/psale/reinoitor/par_aplicatie.c
@@ -1,3 +1,13 @@
+/*
+ * rpsAle - psAle's updater application
+ * Copyright (c) 2012 by Victor ADĂSCĂLIȚEI at admin@tuscale.ro
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/ , read it from the 
+ * COPYING file that resides in the same directory as this file does or send a letter to 
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
+
 #include "par_aplicatie.h"
 
 #include <glib/gprintf.h>
