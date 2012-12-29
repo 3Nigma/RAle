6bdce27679f1bad97985dadb430725a5871a4bfb
Victor ADĂSCĂLIȚEI <adascalitei.victor@gmail.com>
2012-12-29 14:40:08 +0200

    [psale work] Added image resources that are intendet to be used in the manual update process in 'finfo'.

--- src/psale/principal/fl.c
+++ src/psale/principal/fl.c
@@ -16,86 +16,97 @@
 
 GdkPixbuf *
 fl_obtine_imagine_media_scalata(fl_media_type tp, gint w, gint h) {
-  GdkPixbuf *bufImg = NULL;
-  char caleMediaCompleta[256];
-  GError *eroareRaportata = NULL;
-  
-  switch(tp) {
-  case FL_IMG_TUSCALE_LOGO:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FPRINCIPAL "/tuscale_small_logo_ale.png");
-    break;
-    
-  case FL_GENERAL_IMG_FORMULARE:
-    strcpy(caleMediaCompleta, MEDIA_CALE_GENERAL "/psale_img_formulare.png");
-    break;
-   
-  case FL_IMG_FINFO_LICENTA:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/img_licenta.png");
-    break;
-  case FL_IMG_FINFO_INFO:
-    srand(time(NULL));
-    sprintf(caleMediaCompleta, MEDIA_CALE_FINFO "/img_info_%d.png", (rand() % FINFO_INFO_ID_MAX) + FINFO_INFO_ID_MIN);
-    break;
-  case FL_IMG_FINFO_ACTUALIZEAZA:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/bt_icoana_actualizeaza_ap.png");
-    break;
-  case FL_IMG_FINFO_PARASESTE:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/bt_icoana_paraseste_frm.png");
-    break;
-    
-  case FL_IMG_FCOD_TRIMITE_LA_ALE:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_trimite_la_ale.png");
-    break;
-  case FL_IMG_FCOD_COLAPSEAZA:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_colapseaza.png");
-    break;
-  case FL_IMG_FCOD_EXPANDEAZA:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_expandeaza.png");
-    break;
-  case FL_IMG_FCOD_EEPROM:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_eeprom.png");
-    break;
-  case FL_IMG_FCOD_REIA:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_reia.png");
-    break;
-  case FL_IMG_FCOD_SALVEAZA:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_salveaza.png");
-    break;
-  case FL_IMG_FCOD_PARASESTE:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_paraseste.png");
-    break;
-    
-  case FL_IMG_FEEPROM_SCRIE:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FEEPROM "/bt_icoana_scrie_mem.png");
-    break;
-  case FL_IMG_FEEPROM_CITESTE:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FEEPROM "/bt_icoana_citeste_mem.png");
-    break;
-  case FL_IMG_FEEPROM_PARASESTE:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FEEPROM "/bt_icoana_paraseste_frm.png");
-    break;
-    
-  case FL_IMG_FVIZC_PAG_PREC:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FVIZCARTULIE "/bt_icoana_cartulie_pag_precedenta.png");
-    break;
-  case FL_IMG_FVIZC_PAG_URM:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FVIZCARTULIE "/bt_icoana_cartulie_pag_urmatoare.png");
-    break;
-  case FL_IMG_FVIZC_PAG_SALT_CUPRINS:
-    strcpy(caleMediaCompleta, MEDIA_CALE_FVIZCARTULIE "/bt_icoana_cartulie_sari_la_cuprins.png");
-    break;
-    
-  default:
-    /* TODO: nu ar trebui să ajungă aici. Trimite o imagine implicită ? */
-    break;
-  }
-  
-  bufImg = gdk_pixbuf_new_from_file_at_scale(caleMediaCompleta, w, h, FALSE, &eroareRaportata); 
-  if(NULL != eroareRaportata) {
-	g_warning("Nu am putut încărca imaginea '%s'! Motiv : %s", caleMediaCompleta, eroareRaportata->message);
-    g_error_free(eroareRaportata);
-    /* TODO: întoarce o imagine implcită ? */
-  }
-  
-  return bufImg;
+    GdkPixbuf *bufImg = NULL;
+    char caleMediaCompleta[256];
+    GError *eroareRaportata = NULL;
+
+    switch (tp) {
+        case FL_IMG_TUSCALE_LOGO:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FPRINCIPAL "/tuscale_small_logo_ale.png");
+            break;
+
+        case FL_GENERAL_IMG_FORMULARE:
+            strcpy(caleMediaCompleta, MEDIA_CALE_GENERAL "/psale_img_formulare.png");
+            break;
+
+        case FL_IMG_FINFO_LICENTA:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/img_licenta.png");
+            break;
+        case FL_IMG_FINFO_INFO:
+            srand(time(NULL));
+            sprintf(caleMediaCompleta, MEDIA_CALE_FINFO "/img_info_%d.png", (rand() % FINFO_INFO_ID_MAX) + FINFO_INFO_ID_MIN);
+            break;
+        case FL_IMG_FINFO_ACTUALIZEAZA:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/bt_icoana_actualizeaza_ap.png");
+            break;
+        case FL_IMG_FINFO_PARASESTE:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/bt_icoana_paraseste_frm.png");
+            break;
+        case FL_IMG_FINFO_ACTUALIZARE_ESUATA:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/img_actualizare_esuata.png");
+            break;
+        case FL_IMG_FINFO_ACTUALIZARE_SUCCES:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/img_actualizare_succes.png");
+            break;
+        case FL_IMG_FINFO_ACTUALIZARE_ANIM_PROGRES:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/img_actualizare_anim_in_curs.gif");
+            break;
+        case FL_IMG_FINFO_ACTUALIZARE_REPAUS:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FINFO "/img_actualizare_repaus.png");
+            break;
+        case FL_IMG_FCOD_TRIMITE_LA_ALE:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_trimite_la_ale.png");
+            break;
+        case FL_IMG_FCOD_COLAPSEAZA:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_colapseaza.png");
+            break;
+        case FL_IMG_FCOD_EXPANDEAZA:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_expandeaza.png");
+            break;
+        case FL_IMG_FCOD_EEPROM:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_eeprom.png");
+            break;
+        case FL_IMG_FCOD_REIA:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_reia.png");
+            break;
+        case FL_IMG_FCOD_SALVEAZA:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_salveaza.png");
+            break;
+        case FL_IMG_FCOD_PARASESTE:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FCOD "/bt_icoana_paraseste.png");
+            break;
+
+        case FL_IMG_FEEPROM_SCRIE:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FEEPROM "/bt_icoana_scrie_mem.png");
+            break;
+        case FL_IMG_FEEPROM_CITESTE:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FEEPROM "/bt_icoana_citeste_mem.png");
+            break;
+        case FL_IMG_FEEPROM_PARASESTE:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FEEPROM "/bt_icoana_paraseste_frm.png");
+            break;
+
+        case FL_IMG_FVIZC_PAG_PREC:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FVIZCARTULIE "/bt_icoana_cartulie_pag_precedenta.png");
+            break;
+        case FL_IMG_FVIZC_PAG_URM:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FVIZCARTULIE "/bt_icoana_cartulie_pag_urmatoare.png");
+            break;
+        case FL_IMG_FVIZC_PAG_SALT_CUPRINS:
+            strcpy(caleMediaCompleta, MEDIA_CALE_FVIZCARTULIE "/bt_icoana_cartulie_sari_la_cuprins.png");
+            break;
+
+        default:
+            /* TODO: nu ar trebui să ajungă aici. Trimite o imagine implicită ? */
+            break;
+    }
+
+    bufImg = gdk_pixbuf_new_from_file_at_scale(caleMediaCompleta, w, h, FALSE, &eroareRaportata);
+    if (NULL != eroareRaportata) {
+        g_warning("Nu am putut încărca imaginea '%s'! Motiv : %s", caleMediaCompleta, eroareRaportata->message);
+        g_error_free(eroareRaportata);
+        /* TODO: întoarce o imagine implcită ? */
+    }
+
+    return bufImg;
 }
